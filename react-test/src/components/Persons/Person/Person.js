a7f0ce393910e6cbf951459875497f78df615318
Victor <vypl.snf19.@uea.edu.br>
2020-02-26 23:02:59 -0400

    contextType

--- react-test/src/components/Persons/Person/Person.js
+++ react-test/src/components/Persons/Person/Person.js
@@ -12,8 +12,11 @@ class Person extends Component {
     this.inputElRef = React.createRef();
   }
 
+  static contextType = AuthContext;
+
   componentDidMount() {
     this.inputElRef.current.focus();
+    console.log(this.context.authenticated);
   }
 
   render() {
@@ -21,11 +24,11 @@ class Person extends Component {
     const { name, age, children, click, changed } = this.props;
     return (
       <Aux>
-        <AuthContext.Consumer>
-          {context =>
-            context.authenticated ? <p>Authenticated</p> : <p>Please log in</p>
-          }
-        </AuthContext.Consumer>
+        {this.context.authenticated ? (
+          <p>Authenticated</p>
+        ) : (
+          <p>Please log in</p>
+        )}
         <p onClick={click}>
           I am {name} and I am {age} years old
         </p>
