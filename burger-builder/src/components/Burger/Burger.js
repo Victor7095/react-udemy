455d0c6fa02dae9a034614960774cc23aae89bfb
Victor <vypl.snf19.@uea.edu.br>
2020-02-29 15:57:59 -0400

    Dynamic Ouput of Ingredients

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -2,12 +2,18 @@ import React from "react";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from "./Burger.module.css";
 
-const burger = props => {
+const burger = ({ingredients}) => {
+    const transformedIngredients = Object.keys(ingredients)
+        .map(ingredientName => {
+            return [...Array(ingredients[ingredientName])].map((_, i)=>{
+                return <BurgerIngredient key={ingredientName + i} type={ingredientName}></BurgerIngredient>
+            })
+        });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"></BurgerIngredient>
-            <BurgerIngredient type="cheese"></BurgerIngredient>
-            <BurgerIngredient type="meat"></BurgerIngredient>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"></BurgerIngredient>
         </div>
     )
--- burger-builder/src/containers/BurgerBuilder/BurgerBuider.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuider.js
@@ -4,12 +4,20 @@ import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger"
 
 class BurgerBuilder extends Component{
-
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
 
     render() {
+        const {ingredients} = this.state;
         return (
             <Aux>
-                <Burger></Burger>
+                <Burger ingredients={ingredients}></Burger>
                 <div>Build Controls</div>
             </Aux>
         );
