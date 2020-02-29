0fa6aac1f1af86ac9607bdcc89cb26c92e9cda09
Victor <vypl.snf19.@uea.edu.br>
2020-02-29 16:09:35 -0400

    No ingredient text

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -3,12 +3,19 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from "./Burger.module.css";
 
 const burger = ({ingredients}) => {
-    const transformedIngredients = Object.keys(ingredients)
+    let transformedIngredients = Object.keys(ingredients)
         .map(ingredientName => {
             return [...Array(ingredients[ingredientName])].map((_, i)=>{
                 return <BurgerIngredient key={ingredientName + i} type={ingredientName}></BurgerIngredient>
-            })
-        });
+            });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+        console.log(transformedIngredients)
+        if (transformedIngredients.length === 0) {
+            transformedIngredients = <p>Please start adding in some ingredients!</p>
+        }
 
     return (
         <div className={classes.Burger}>
