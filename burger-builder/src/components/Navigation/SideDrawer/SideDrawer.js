bb6d5a7a42cb31543831a8d0c5905ab7334d630a
Victor <vypl.snf19.@uea.edu.br>
2020-03-26 19:02:43 -0400

    SideDrawer Logo responsive

--- burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -5,7 +5,9 @@ import classes from "./SideDrawer.module.css";
 const sideDrawer = props => {
     return (
         <div className={classes.SideDrawer}>
-            <Logo/>
+            <div className={classes.Logo}>
+                <Logo/>
+            </div>
             <nav>
                 <NavigationItems/>
             </nav>
--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -6,9 +6,11 @@ import classes from "./Toolbar.module.css";
 const toolbar = props => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <Logo/>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
         <nav>
-            <NavigationItems/>
+            <NavigationItems />
         </nav>
     </header>
 );
