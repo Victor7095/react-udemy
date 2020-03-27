676edb8be24874627ee8751ed1766fdf68efa47d
Victor <vypl.snf19.@uea.edu.br>
2020-03-27 07:32:21 -0400

    Modal optimization

--- burger-builder/src/components/UI/Modal/Modal.js
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Aux from "../../../hoc/Aux";
 import Backdrop from "../Backdrop/Backdrop";
 import classes from "./Modal.module.css";
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed}></Backdrop>
-        <div 
-            className={classes.Modal}
-            style={{
-                transform: props.show ? "translateY(0)" : "translateY(-100vh" ,
-                opacity: props.show ? "1" : "0"
-        }}>
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends Component {
 
-export default modal;
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    render(){
+        const {show, modalClosed, children} = this.props;
+        return(
+            <Aux>
+                <Backdrop show={show} clicked={modalClosed}></Backdrop>
+                <div 
+                    className={classes.Modal}
+                    style={{
+                        transform: show ? "translateY(0)" : "translateY(-100vh" ,
+                        opacity: show ? "1" : "0"
+                }}>
+                    {children}
+                </div>
+            </Aux>
+        );
+    }
+}
+
+export default Modal;
