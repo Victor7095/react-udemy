f715e71a62e37c93b55ef233beb4cf0e86270ac1
Victor <vypl.snf19.@uea.edu.br>
2020-04-07 14:00:49 -0400

    Installed axios

--- http--01-starting-setup/src/components/Post/Post.js
+++ http--01-starting-setup/src/components/Post/Post.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './Post.css';
 
-const post = (props) => (
+const post = ({title, author}) => (
     <article className="Post">
-        <h1>Title</h1>
+        <h1>{title}</h1>
         <div className="Info">
-            <div className="Author">Author</div>
+            <div className="Author">{author}</div>
         </div>
     </article>
 );
--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,26 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount () {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(res => {
+            this.setState({posts: res.data});
+        });
+    }
+
     render () {
+        const posts = this.state.posts.map(post => {
+            return (<Post key={post.id} title={post.title} author={post.author}/>)
+        });
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    { posts }
                 </section>
                 <section>
                     <FullPost />
