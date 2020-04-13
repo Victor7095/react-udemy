7bd38857f84344b4815822e9e5d429dc7a087b9d
Victor <vypl.snf19.@uea.edu.br>
2020-04-13 17:44:27 -0400

    Fetch post correct

--- http--01-starting-setup/src/components/FullPost/FullPost.js
+++ http--01-starting-setup/src/components/FullPost/FullPost.js
@@ -1,17 +1,35 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import "./FullPost.css";
 
 class FullPost extends Component {
+  state = {
+    loadedPost: null
+  }
+  componentDidUpdate() {
+    const { id } = this.props;
+    const { loadedPost } = this.state;
+    if (id && (!loadedPost || (loadedPost && loadedPost.id !== id))) {
+      axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then(res => {
+        this.setState({loadedPost: res.data});
+      });
+    }
+  }
 
   render() {
+    const { loadedPost } = this.state;
     const { id } = this.props;
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
     if (id) {
+      post = <p style={{ textAlign: "center" }}>Loading!</p>;
+    }
+    if (loadedPost) {
       post = (
         <div className="FullPost">
-          <h1>Title</h1>
-          <p>Content</p>
+          <h1>{loadedPost.title}</h1>
+          <p>{loadedPost.body}</p>
           <div className="Edit">
             <button className="Delete">Delete</button>
           </div>
