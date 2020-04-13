55eb7ab8051294e35ea16808131473c3a161aa1f
Victor <vypl.snf19.@uea.edu.br>
2020-04-13 18:58:53 -0400

    axios interceptors

--- http--01-starting-setup/src/index.js
+++ http--01-starting-setup/src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(req => {
+  console.log(req);
+  return req;
+}, error => {
+  console.log(error);
+  return Promise.reject(error)
+});
+
+axios.interceptors.response.use(res => {
+  console.log(res);
+  return res;
+}, error => {
+  console.log(error);
+  return Promise.reject(error)
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
