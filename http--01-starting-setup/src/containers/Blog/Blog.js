f86256474338422f51ee8ce649104139ebb8a584
Victor <vypl.snf19.@uea.edu.br>
2020-04-16 16:28:34 -0400

    Redirect requests

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch, NavLink } from "react-router-dom";
+import { Route, Switch, NavLink, Redirect } from "react-router-dom";
 
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
@@ -27,6 +27,7 @@ class Blog extends Component {
         <Switch>
           <Route path="/new-post" component={NewPost} />
           <Route path="/posts" component={Posts} />
+          <Redirect from="/" to="/posts" />
         </Switch>
         {/* <Route path="/post/:id" component={FullPost} /> */}
       </div>
