10f96880b10090688e037730efd6cebec5699d9d
Victor <vypl.snf19.@uea.edu.br>
2020-04-16 10:44:00 -0400

    Setting home and new post routes

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { Route } from "react-router-dom";
 
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+
 import './Blog.css';
 
 class Blog extends Component {
@@ -16,7 +19,8 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Posts />
+        <Route path="/" component={Posts} exact />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
