6a4d3259434bc3af604d01476f8bf244dc8f174c
Victor <vypl.snf19.@uea.edu.br>
2020-04-16 10:32:47 -0400

    Posts component

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,52 +1,11 @@
 import React, { Component } from 'react';
-import axios from '../../axios';
 
-import Post from '../../components/Post/Post';
+import Posts from './Posts/Posts';
 import './Blog.css';
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false,
-  };
-
-  componentDidMount() {
-    axios
-      .get('/posts')
-      .then((res) => {
-        const posts = res.data.slice(0, 4);
-        const updatedPosts = posts.map((post) => {
-          return {
-            ...post,
-            author: "Yan",
-          };
-        });
-        this.setState({ posts: updatedPosts });
-      })
-      .catch((err) => {
-        this.setState({ error: true });
-      });
-  }
-
-  postSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id });
-  };
 
   render() {
-    let posts = <p style={{ textAlign: "center" }}>Something went wrong</p>;
-    if (!this.state.error)
-      posts = this.state.posts.map((post) => {
-        return (
-          <Post
-            key={post.id}
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}
-          />
-        );
-      });
-
     return (
       <div className="Blog">
         <header>
@@ -57,7 +16,7 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <section className="Posts">{posts}</section>
+        <Posts />
       </div>
     );
   }
