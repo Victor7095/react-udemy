74331fa9221876b178c5eb6d022cb8fd11c40152
Victor <vypl.snf19.@uea.edu.br>
2020-04-16 10:52:29 -0400

    Using router link

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -14,8 +14,8 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/new-post">New Post</a></li>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/new-post">New Post</Link></li>
             </ul>
           </nav>
         </header>
