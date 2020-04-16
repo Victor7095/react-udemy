8bf3102434f1a2526527cf41852a12f557e4330c
Victor <vypl.snf19.@uea.edu.br>
2020-04-16 10:05:52 -0400

    Adding router to posts project

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import axios from '../../axios';
 
 import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -60,12 +58,6 @@ class Blog extends Component {
           </nav>
         </header>
         <section className="Posts">{posts}</section>
-        <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
       </div>
     );
   }
