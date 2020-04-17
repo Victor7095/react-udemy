d254c4b78b3cfc0d5738a42c085a5406c5d5a3ea
Victor <vypl.snf19.@uea.edu.br>
2020-04-17 11:06:31 -0400

    Adding routes to burger project

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuider";
 import Checkout from "./containers/Checkout/Checkout";
@@ -7,8 +9,10 @@ function App() {
   return (
     <div>
       <Layout>
-        <BurgerBuilder />
-        <Checkout />
+        <Switch>
+          <Route path="/" exact component={BurgerBuilder} />
+          <Route path="/checkout" component={Checkout} />
+        </Switch>
       </Layout>
     </div>
   );
--- burger-builder/src/hoc/Layout/Layout.js
+++ burger-builder/src/hoc/Layout/Layout.js
@@ -5,33 +5,34 @@ import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 import classes from "./Layout.module.css";
 
-class Layout extends Component{
-    state = {
-        showSideDrawer: false
-    };
+class Layout extends Component {
+  state = {
+    showSideDrawer: false,
+  };
 
-    sideDrawerClosedHandler = () => {
-        this.setState({showSideDrawer: false})
-    }
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
 
-    sideDrawerToggleHandler = () => {
-        this.setState(prevState => ({showSideDrawer: !prevState.showSideDrawer}))
-    }
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => ({
+      showSideDrawer: !prevState.showSideDrawer,
+    }));
+  };
 
-    render(){
-        const {showSideDrawer} = this.state;
-        return(
-            <Aux>
-                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
-                <SideDrawer
-                    open={showSideDrawer}
-                    closed={this.sideDrawerClosedHandler}/>
-                <main className={classes.Content}>
-                    {this.props.children}
-                </main>
-            </Aux>
-        );
-    }
+  render() {
+    const { showSideDrawer } = this.state;
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          open={showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
 }
 
 export default Layout;
