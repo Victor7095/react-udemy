ea4c10e49e59dfef8151262f61bf45dbb0d49525
Victor <vypl.snf19.@uea.edu.br>
2020-04-19 16:40:07 -0400

    Adding subscriptions

--- redux--01-start/redux-basics.js
+++ redux--01-start/redux-basics.js
@@ -2,28 +2,30 @@ const redux = require("redux");
 const createStore = redux.createStore;
 
 const initialState = {
-  counter: 0
-}
+  counter: 0,
+};
 
 const rootReducer = (state = initialState, action) => {
   if (action.type === "INC_COUNTER") {
     return {
       ...state,
-      counter: ++state.counter
-    }
+      counter: ++state.counter,
+    };
   }
   if (action.type === "ADD_COUNTER") {
     return {
       ...state,
-      counter: state.counter + action.value
-    }
+      counter: state.counter + action.value,
+    };
   }
   return state;
-}
+};
 
 const store = createStore(rootReducer);
-console.log(store.getState());
 
-store.dispatch({type: "INC_COUNTER"});
-store.dispatch({type: "ADD_COUNTER", value: 10});
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
