5631ade2df562a9ebd3ec38c35f8c37c76c941e5
Victor <vypl.snf19.@uea.edu.br>
2020-04-19 16:37:42 -0400

    Dispatching actions

--- redux--01-start/redux-basics.js
+++ redux--01-start/redux-basics.js
@@ -6,8 +6,24 @@ const initialState = {
 }
 
 const rootReducer = (state = initialState, action) => {
+  if (action.type === "INC_COUNTER") {
+    return {
+      ...state,
+      counter: ++state.counter
+    }
+  }
+  if (action.type === "ADD_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    }
+  }
   return state;
 }
 
 const store = createStore(rootReducer);
-console.log(store.getState())
+console.log(store.getState());
+
+store.dispatch({type: "INC_COUNTER"});
+store.dispatch({type: "ADD_COUNTER", value: 10});
+console.log(store.getState());
