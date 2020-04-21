65c18c07fe63ec327e4f797637c0034a4480d27d
Victor <vypl.snf19.@uea.edu.br>
2020-04-21 14:56:09 -0400

    Passing data to reducer

--- redux--01-start/src/containers/Counter/Counter.js
+++ redux--01-start/src/containers/Counter/Counter.js
@@ -44,15 +44,15 @@ class Counter extends Component {
         />
         <CounterControl
           label="Decrement"
-          clicked={() => this.counterChangedHandler("dec")}
+          clicked={this.props.onDecrementCounter}
         />
         <CounterControl
           label="Add 5"
-          clicked={() => this.counterChangedHandler("add", 5)}
+          clicked={this.props.onAddCounter}
         />
         <CounterControl
           label="Subtract 5"
-          clicked={() => this.counterChangedHandler("sub", 5)}
+          clicked={this.props.onSubtractCounter}
         />
       </div>
     );
@@ -68,6 +68,10 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
+    onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
+    onAddCounter: () => dispatch({ type: "ADD", payload: { value: 5 } }),
+    onSubtractCounter: () =>
+      dispatch({ type: "SUBTRACT", payload: { value: 5 } }),
   };
 };
 
--- redux--01-start/src/store/reducer.js
+++ redux--01-start/src/store/reducer.js
@@ -1,14 +1,27 @@
 const initialState = {
-  counter: 0
-}
+  counter: 0,
+};
 
-const reducer = (state = initialState, action) => {
-  if(action.type == "INCREMENT") {
-    return {
+const reducer = (state = initialState, {type, payload}) => {
+  const actions = {
+    "INCREMENT": () => ({
       ...state,
-      counter: ++state.counter
-    }
-  }
+      counter: ++state.counter,
+    }),
+    "DECREMENT": () => ({
+      ...state,
+      counter: --state.counter,
+    }),
+    "ADD": () => ({
+      ...state,
+      counter: state.counter + payload.value,
+    }),
+    "SUBTRACT": () => ({
+      ...state,
+      counter: state.counter - payload.value,
+    }),
+  };
+  if(actions[type]) return actions[type]();
   return state;
 };
 
