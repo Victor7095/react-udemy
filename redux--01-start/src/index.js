e8872cfd6daf3c6bb26e1c94982bfc2cf09a47d6
Victor <vypl.snf19.@uea.edu.br>
2020-04-22 17:40:58 -0400

    Adding redux dev tools

--- redux--01-start/src/index.js
+++ redux--01-start/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 
 import "./index.css";
@@ -14,18 +14,22 @@ const rootReducer = combineReducers({
   res: resultReducer,
 });
 
-const logger = store => {
-  return next => {
-    return action => {
+const logger = (store) => {
+  return (next) => {
+    return (action) => {
       console.log("[Middleware] Dispatching", action);
       const result = next(action);
       console.log("[Middleware] next state", store.getState());
       return result;
-    }
-  }
-}
+    };
+  };
+};
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger))
+);
 
 ReactDOM.render(
   <Provider store={store}>
