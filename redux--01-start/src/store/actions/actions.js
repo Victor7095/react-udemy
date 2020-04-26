99e9d06c8dfc119317970b9df49b32f18f8191f9
Victor <vypl.snf19.@uea.edu.br>
2020-04-26 08:40:54 -0400

    Asynchronous code

--- redux--01-start/src/store/actions/actions.js
+++ redux--01-start/src/store/actions/actions.js
@@ -31,11 +31,20 @@ export const subtract = (value) => {
   };
 };
 
-export const storeResult = (result) => {
+export const saveResult = result => {
   return {
     type: STORE_RESULT,
     payload: { result },
   };
+}
+
+export const storeResult = (result) => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(saveResult(result));
+    }, 2000);
+  }
+  
 };
 
 export const deleteResult = (index) => {
