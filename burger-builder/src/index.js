017d5f738399249ef97fdd793dbddf2a8187c5a0
Victor <vypl.snf19.@uea.edu.br>
2020-04-26 14:41:39 -0400

    Preparing Folder Structure

--- burger-builder/src/index.js
+++ burger-builder/src/index.js
@@ -7,7 +7,7 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import reducer from "./store/reducer";
+import reducer from "./store/reducers/burgerBuilder";
 
 const store = createStore(
   reducer,
