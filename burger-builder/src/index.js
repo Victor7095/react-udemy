cbc4ed2ffd2c8db53c539f9f159885359f8db4ba
Victor <vypl.snf19.@uea.edu.br>
2020-04-26 14:18:17 -0400

    Reduz dev tools in burger builder

--- burger-builder/src/index.js
+++ burger-builder/src/index.js
@@ -9,7 +9,11 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import reducer from "./store/reducer";
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const app = (
   <Provider store={store}>
