1ab2a5fe40ea3f5b683405301eb205e903392e33
Victor Yan Pereira e Lima <36238607+Victor7095@users.noreply.github.com>
2020-07-09 11:50:31 -0400

    Second Test Case

--- burger-builder/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ burger-builder/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -8,8 +8,19 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 configure({ adapter: new Adapter() });
 
 describe("<NavigationItems />", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+
   it("should render 2 <NavigationItem /> elements if not authenticated", () => {
-    const wrapper = shallow(<NavigationItems />);
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+
+  it("should render 3 <NavigationItem /> elements if authenticated", () => {
+    wrapper.setProps({
+      isAuthenticated: true
+    })
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 });
