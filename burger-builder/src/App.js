e5a9e43a9b878f4eea81d7e44cbd78ab8f8028e0
Victor Yan Pereira e Lima <36238607+Victor7095@users.noreply.github.com>
2020-07-08 21:53:39 -0400

    Adding Lazy Loading

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Route, Switch, Redirect, withRouter } from "react-router-dom";
+import asyncComponent from "./hoc/asyncComponent/asyncComponent";
 
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuider";
-import Checkout from "./containers/Checkout/Checkout";
-import Orders from "./containers/Orders/Orders";
-import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
 import { authCheckState } from "./store/actions/";
+
+const asyncCheckout = asyncComponent(() =>
+  import("./containers/Checkout/Checkout")
+);
+
+const asyncOrders = asyncComponent(() => import("./containers/Orders/Orders"));
+
+const asyncAuth = asyncComponent(() => import("./containers/Auth/Auth"));
 
 class App extends Component {
   componentDidMount() {
@@ -18,7 +24,7 @@ class App extends Component {
   render() {
     let routes = (
       <Switch>
-        <Route path="/auth" component={Auth} />
+        <Route path="/auth" component={asyncAuth} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
       </Switch>
@@ -27,10 +33,10 @@ class App extends Component {
       routes = (
         <Switch>
           <Route path="/" exact component={BurgerBuilder} />
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
+          <Route path="/checkout" component={asyncCheckout} />
+          <Route path="/orders" component={asyncOrders} />
           <Route path="/logout" component={Logout} />
-          <Route path="/auth" component={Auth} />
+          <Route path="/auth" component={asyncAuth} />
           <Redirect to="/" />
         </Switch>
       );
