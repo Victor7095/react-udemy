cb723384c03db446ed5fec5b7cb803e57e599f5b
Victor Yan Pereira e Lima <36238607+Victor7095@users.noreply.github.com>
2020-07-17 13:21:50 -0400

    Sending http requests

--- hooks-01-starting-project/src/components/Ingredients/IngredientList.js
+++ hooks-01-starting-project/src/components/Ingredients/IngredientList.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import './IngredientList.css';
+import "./IngredientList.css";
 
-const IngredientList = props => {
+const IngredientList = ({ ingredients, onRemoveItem }) => {
   return (
     <section className="ingredient-list">
       <h2>Loaded Ingredients</h2>
       <ul>
-        {props.ingredients.map(ig => (
-          <li key={ig.id} onClick={props.onRemoveItem.bind(this, ig.id)}>
+        {ingredients.map((ig) => (
+          <li key={ig.id} onClick={onRemoveItem.bind(this, ig.id)}>
             <span>{ig.title}</span>
             <span>{ig.amount}x</span>
           </li>
--- hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import uuid from "uuid";
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import Search from "./Search";
@@ -7,20 +6,44 @@ import Search from "./Search";
 function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
 
-  const addIngredientHandler = (ingredient) => {
-    setIngredients((oldIngredients) => [
-      ...oldIngredients,
-      { id: uuid(), ...ingredient },
-    ]);
+  const addIngredientHandler = (newIngredient) => {
+    fetch(
+      "https://react-hooks-c609e.firebaseio.com/ingredients.json?apiKey=AIzaSyAijVUrsKW3EUpw_K11N2L0-dMyoGhjsYA",
+      {
+        method: "POST",
+        body: JSON.stringify(newIngredient),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((body) => {
+        setIngredients((oldIngredients) => [
+          ...oldIngredients,
+          { id: body.name, ...newIngredient },
+        ]);
+      });
+  };
+
+  const removeIngredientHandler = (id) => {
+    setIngredients((oldIngredients) =>
+      oldIngredients.filter((ig) => id !== ig.id)
+    );
   };
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler}/>
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        <IngredientList ingredients={ingredients} onRemoveItem={() => {}} />
+        <IngredientList
+          ingredients={ingredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
