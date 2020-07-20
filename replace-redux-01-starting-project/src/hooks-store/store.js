580d92aa654056952ef68c253fe4e311336b0b62
Victor Yan Pereira e Lima <36238607+Victor7095@users.noreply.github.com>
2020-07-20 15:17:38 -0400

    Created custom hook store

---
+++ replace-redux-01-starting-project/src/hooks-store/store.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+let globalState = {};
+
+let listeners = [];
+
+let actions = {};
+
+export const useStore = () => {
+  const setState = useState(globalState)[1];
+
+  const dispatch = (action) => {
+    const newState = actions[action](globalState);
+    globalState = { ...globalState, ...newState };
+
+    for (const listener of listeners) {
+      listener(globalState);
+    }
+  };
+
+  useEffect(() => {
+    listeners.push(setState);
+    return () => {
+      listeners = listeners.filter((listener) => listener !== setState);
+    };
+  }, [setState]);
+
+  return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+  if (initialState) globalState = { ...initialState };
+  actions = { ...actions, ...userActions };
+};
